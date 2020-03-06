610b314a4e51b68e4fe4a818926e12b8a2f3dc48
meet.patel <meet.patel@DTA158.specindia.com>
2020-03-06 18:02:20 +0530

    first commit

---
+++ DemoSpringFlyWay/src/main/java/com/flyway/demo/DemoSpringFlyWayApplication.java
@@ -0,0 +1,13 @@
+package com.flyway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringFlyWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringFlyWayApplication.class, args);
+	}
+
+}
---
+++ DemoSpringFlyWay/src/main/java/com/flyway/model/User.java
@@ -0,0 +1,28 @@
+package com.flyway.model;
+
+import javax.annotation.sql.DataSourceDefinition;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="REGISTRATION_USERS")
+public class User {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String username;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String mobile;
+	
+	
+	
+	
+
+}
