08d4bde78b9d635b0789971e3fd5bdf0eca97adb
meet.patel <meet.patel@DTA158.specindia.com>
2020-03-09 09:27:34 +0530

    first commit

---
+++ DemoSpringBootJPa/src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,69 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EmployeeController {
+	@Autowired
+	EmployeeService service;
+
+	@GetMapping("/")
+	public String getAllEmployees(Model model) {
+		List<Employee> list = service.listAll();
+		model.addAttribute("employees", list);
+		return "list-employees";
+	}
+
+	@RequestMapping("/new")
+	public String showNewEmployeePage(Model model) {
+		Employee employee = new Employee();
+		model.addAttribute("employee", employee);
+
+		return "add-edit-employee";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveProduct(@ModelAttribute("employee") Employee employee) {
+//		service.createOrUpdateEmployee(employee);
+		service.save(employee);
+		return "redirect:/";
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+		Employee entity = service.getEmployeeById(id);
+
+		return new ResponseEntity<Employee>(entity, new HttpHeaders(), HttpStatus.OK);
+	}
+
+	@RequestMapping("/delete/{id}")
+	public String deleteEmployeeById(@PathVariable(name = "id") long id) {
+		service.delete(id);
+		return "redirect:/";
+	}
+
+	@RequestMapping("/edit/{id}")
+	public ModelAndView showEditEmployeePage(@PathVariable(name = "id") long id) {
+		ModelAndView mav = new ModelAndView("edit-employee");
+		Employee employee = service.getEmployeeById(id);
+		mav.addObject("employee", employee);
+
+		return mav;
+	}
+}
---
+++ DemoSpringBootJPa/src/main/java/com/example/demo/EmployeeService.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	EmployeeRepository repository;
+
+//    public List<Employee> getAllEmployees()
+//    {
+//        List<Employee> employeeList = repository.findAll();
+//         
+//       return employeeList;
+//    }
+	public List<Employee> listAll() {
+		return repository.findAll();
+	}
+
+	public Employee getEmployeeById(Long id) {
+		Optional<Employee> employee = repository.findById(id);
+
+		if (employee.isPresent()) {
+			return employee.get();
+		} else {
+			System.out.println("No employee record exist for given id");
+			return null;
+		}
+	}
+
+	public void save(Employee entity) {
+		repository.save(entity);
+	}
+
+	public void delete(long id) {
+		repository.deleteById(id);
+	}
+
+//    public void deleteEmployeeById(Long id)
+//    {
+//        Optional<Employee> employee = repository.findById(id);
+//         
+//        if(employee.isPresent()) 
+//        {
+//            repository.deleteById(id);
+//        } else {
+//            System.out.println("No employee record exist for given id");
+//        }
+//    } 
+
+}
